63fccc344fbf820744c42aa0f13b80d4ee750cb9
anupBH <anupbhaumik44@gmail.com>
2023-09-23 20:11:21 +0530

    d

--- app/pages/api/getlisting/route.ts
+++
@@ -1,8 +0,0 @@
-
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//    const listingitems= await prisma?.listing.findMany();
-//    return NextResponse.json(listingitems); 
-    
-// }
